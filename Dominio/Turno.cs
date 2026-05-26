63142c34bea27607462d9592f037578ed243c848
Leonardo <leomicale.lm@gmail.com>
2026-05-26 20:51:23 -0300

    Creación clases Turno y EstadoTurno

---
+++ Dominio/Turno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Turno
+    {
+        public int IdTurno { get; set; } //Ver si es necesario crear un nro de turno o se utiliza este (autonumerado)
+        public DateTime Fecha { get; set; }
+        public TimeSpan Hora { get; set; }
+        public string Observaciones { get; set; } //Motivo del turno
+        public string Diagnostico { get; set; } //Resultado de la consulta
+    }
+}
