5263cee8a51688f83a415e28ec961e00995a00d3
Ivan Eloy Aguilera <ivaneaguilera96@gmail.com>
2026-05-25 21:52:41 -0300

    Creacion de clase Medico y Especialidad

---
+++ Dominio/Medico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Medico
+    {
+        public int IdMedico { get; set; }
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Matricula { get; set; }
+
+    }
+}
