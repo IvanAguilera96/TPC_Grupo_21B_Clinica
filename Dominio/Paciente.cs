3aeac4235a5f76bf1ab4601c031b2e2df0767833
Ivan Eloy Aguilera <ivaneaguilera96@gmail.com>
2026-05-25 21:45:56 -0300

    Creacion de clase Paciente

---
+++ Dominio/Paciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Paciente
+    {
+        public int IdPaciente { get; set; }
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
