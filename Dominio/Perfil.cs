2f4726db40ac43ee726d024988a30c1f2d43822c
Leonardo <leomicale.lm@gmail.com>
2026-05-26 20:28:13 -0300

    Creación de clases Perfil y Usuario

---
+++ Dominio/Perfil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Perfil
+    {
+        public int IdPerfil { get; set; }
+        public string Descripcion { get; set; } //Médico, Recepcionista, etc. Rol asociado al usuario
+    }
+}
