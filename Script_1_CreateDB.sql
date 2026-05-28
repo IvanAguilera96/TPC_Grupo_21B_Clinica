d4c01a2dc56ed0950a7f49bd497653f4bdfbb639
Ivan Eloy Aguilera <ivaneaguilera96@gmail.com>
2026-05-28 15:10:31 -0300

    Creacion de Script Create DB mas carpeta

---
+++ Script_1_CreateDB.sql
@@ -0,0 +1,62 @@
+﻿CREATE DATABASE TPC_Clinica;
+GO
+USE TPC_Clinica;
+GO
+
+ CREATE TABLE Medico(
+	IdMedico INT PRIMARY KEY IDENTITY(1,1),
+	Dni VARCHAR(8) NOT NULL,
+	Nombre VARCHAR(50) NOT NULL,
+	Apellido VARCHAR(50) NOT NULL,
+	Matricula INT UNIQUE NOT NULL
+);
+
+CREATE TABLE Paciente(
+	IdPaciente INT PRIMARY KEY IDENTITY(1,1),
+	Dni VARCHAR(8) NOT NULL,
+	Nombre VARCHAR(50) NOT NULL,
+	Apellido VARCHAR(50) NOT NULL,
+	Email VARCHAR(50) NOT NULL,
+	Telefono VARCHAR(20) NULL,
+);
+
+CREATE TABLE Especialidad(
+	IdEspecialidad INT PRIMARY KEY IDENTITY(1,1),
+	Descripcion VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE TurnoTrabajo(
+	IdTurnoTrabajo INT PRIMARY KEY IDENTITY(1,1),
+	Descripcion VARCHAR(100) NOT NULL,
+	HoraEntrada Time NOT NULL,
+	HoraSalida Time NOT NULL
+);
+
+CREATE TABLE EstadoTurno(
+	IdEstadoTurno INT PRIMARY KEY IDENTITY(1,1),
+	Descripcion VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Turno(
+	IdTurno INT PRIMARY KEY IDENTITY(1,1),
+	Fecha DateTime NOT NULL,
+	Hora Time NOT NULL,
+	IdMedico INT FOREIGN KEY REFERENCES Medico(IdMedico),
+	IdEspecialidad INT FOREIGN KEY REFERENCES Especialidad(IdEspecialidad),
+	IdPaciente INT FOREIGN KEY REFERENCES Paciente(IdPaciente),
+	IdEstadoTurno INT FOREIGN KEY REFERENCES EstadoTurno(IdEstadoTurno),
+	Observacion VARCHAR(100) NOT NULL,
+	Diagnostico VARCHAR(100) NULL
+);
+
+CREATE TABLE Perfil(
+	IdPerfil INT PRIMARY KEY IDENTITY(1,1),
+	Descripcion VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Usuario(
+	IdUsuario INT PRIMARY KEY IDENTITY(1,1),
+	Nombre VARCHAR(50) NOT NULL,
+	Contrasenia VARCHAR(20) NOT NULL,
+	IdPerfil INT FOREIGN KEY REFERENCES Perfil(IdPerfil)
+);
